9df9f2bd339cbc9102bd3f8e540d04c113e836e0
anthony reddy <anthonyreddy21@gmail.com>
2022-07-13 17:54:57 +0530

    added bit_diff with minor changes

--- Desktop/my-practice/bit_diff.c
+++ Desktop/my-practice/bit_diff.c
@@ -2,6 +2,31 @@
 
 // WAP different bits in given two numbers.
 
+// 1. using main()
+
+#include<stdio.h>
+
+int main()
+{
+    int a = 0, b = 0, count = 0;
+    
+    printf("enter 1st number\n");
+    scanf("%d",&a);
+    
+    printf("enter 2nd number\n");
+    scanf("%d",&b);
+ 
+    for (int i = 0; i < 32; i++) 
+    {
+        if (((a >> i) & 1) != ((b >> i) & 1)) 
+            count++;
+    }
+ 
+    return 0;
+}
+
+// 2. using function 
+
 #include<stdio.h>
 
 void bit_diff(int a, int b)
